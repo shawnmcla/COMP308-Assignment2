760624893e64e4e86db41735f42ea7b5a43e18fe
Shawn <shawnmclau@gmail.com>
2017-03-10 14:40:37 -0500

    implemented authentication

--- COMP308-Assignment1/server/routes/index.js
+++ COMP308-Assignment1/server/routes/index.js
@@ -5,33 +5,118 @@
  * Description: Routing instructions for URLs
  */
 
+// Modules for routing
 let express = require('express');
 let router = express.Router();
+let mongoose = require('mongoose');
+let passport = require('passport');
+
+// define user models
+let UserModel = require('../models/users');
+let User = UserModel.User;
+
+/*
+// define game model
+let game = require('../models/games');
+*/
+
+// function to check if the user is authenticated
+function requireAuth(req, res, next) {
+  //check if user is logged in
+  if (!req.isAuthenticated()) {
+    return res.redirect('/login');
+  }
+  next();
+}
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
   let currentDate = new Date();
-  res.render('content/index', { title: 'Home' });
+  res.render('content/index', {
+    title: 'Home',
+    displayName: req.user ? req.user.displayName : ''
+  });
 });
 
-/* GET about page. */
-router.get('/about', (req, res, next) => {
-  res.render('content/about', { title: 'About' });
-});
-
-/* GET services page. */
-router.get('/services', (req, res, next) => {
-  res.render('content/services', { title: 'Services' });
-});
-
-/* GET projects page. */
-router.get('/projects', (req, res, next) => {
-  res.render('content/projects', { title: 'Projects' });
-});
-
-/* GET contact page. */
+/* GET contact page */
 router.get('/contact', (req, res, next) => {
-  res.render('content/contact', { title: 'Contact' });
+  res.render('content/contact', {
+    title: 'Contact',
+    displayName: req.user ? req.user.displayName : ''
+  });
+});
+
+/* GET login page */
+router.get('/login', (req, res, next) => {
+  //check to see if user is not already logged in
+  if (!req.user) {
+    res.render('auth/login', {
+      title: 'Login',
+      messages: req.flash('error'),
+      displayName: req.user ? req.user.displayName : ''
+    });
+    return;
+  } else {
+    return res.redirect('/'); // redirect to index 
+  }
+});
+// POST /login
+router.post('/login', passport.authenticate('local', {
+  successRedirect: '/',
+  failureRedirect: '/login',
+  failureFlash: true
+}));
+
+// GET /register
+router.get('/register', (req, res, next) => {
+  //check if user not already logged in
+  if (!req.user) {
+    res.render('auth/register', {
+      title: 'Register',
+      messages: req.flash('registerMessage'),
+      displayName: req.user ? req.user.displayName : ''
+    });
+  }
+  else {
+    res.redirect('/');
+  }
+});
+
+// POST /register
+router.post('/register', (req, res, next) => {
+  User.register(
+    new User(
+      {
+        username: req.body.username,
+        //password: req.body.password,
+        email: req.body.email,
+        displayName: req.body.displayName
+      }),
+    req.body.password,
+    (err) => {
+      if (err) {
+        console.log('Error registering user.');
+        console.log(err);
+        if (err.name == "UserExistsError") {
+          req.flash('registerMessage', "Registration Error: User already exists!");
+        }
+        res.render('auth/register', {
+          title: 'Register',
+          messages: req.flash('registerMessage'),
+          displayName: req.user ? req.user.displayName : ''
+        });
+      }
+      // if registration is successful
+      return passport.authenticate('local')(req, res, () => {
+        res.redirect('/');
+      });
+    });
+});
+
+// GET /logout - logout the user and redir. to homepage
+router.get('/logout', (req, res, next) => {
+  req.logout();
+  res.redirect('/');
 });
 
 module.exports = router;
